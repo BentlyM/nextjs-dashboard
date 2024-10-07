9c777c76e04a76e140eb3dcf2117e0d7fd5e396d
BentlyM <metayerbently@gmail.com>
2024-10-06 20:24:09 -0400

    "Added global CSS and font imports, updated layout and page components with new styles and images, and modified navigation links with conditional styling."

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter} from 'next/font/google';
+import { Lusitana as Lusitania } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitania = Lusitania({subsets: ['latin'], weight: ['400','700']});
